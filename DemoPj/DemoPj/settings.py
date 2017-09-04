1ee8fdd79f3126c5051e1065edb31338d2bb6293
hulianghai <hulianghai@xmzhen.com>
2017-09-04 11:09:45 +0800

    fix

--- DemoPj/DemoPj/settings.py
+++ DemoPj/DemoPj/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '46y-0y1fl)k$@@+ma8eq4_w$@2pn4naatw$0n(jm+v0s+l0n^b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '47.94.0.190']
 
 # Application definition
 
