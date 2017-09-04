a5c3d8b50d2e5ada631f56dd380c1184fd216b84
hulianghai <hulianghai@xmzhen.com>
2017-09-04 11:07:08 +0800

    iii * 简单描述一下干了什么 What:
    * 为什么要这么做
    Why:
    
    * 怎样做的？有何副作用？
    How:☺

--- DemoPj/DemoPj/settings.py
+++ DemoPj/DemoPj/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '46y-0y1fl)k$@@+ma8eq4_w$@2pn4naatw$0n(jm+v0s+l0n^b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
